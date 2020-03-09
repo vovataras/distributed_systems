90dd4192bbd1c4ff2c6d171dfcd96973fc0f32f6
Volodya Taras <proger.sdk@gmail.com>
2020-03-09 12:00:23 +0200

    "Lab_3: Update client.tcp"
    This reverts commit 6825c9284cbd07cdb2686566188056a220c33fee.

--- lab3/client.tcp/src/main/java/client/tcp/Main.java
+++ lab3/client.tcp/src/main/java/client/tcp/Main.java
@@ -1,11 +1,22 @@
 package client.tcp;
 
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        try (TcpClient client = new TcpClient(args)) {
+        TcpClient client = new TcpClient(args);
+
+        try {
             client.run();
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (client != null){
+            client.close();
+
         }
     }
 }
--- lab3/client.tcp/src/main/java/client/tcp/TcpClient.java
+++ lab3/client.tcp/src/main/java/client/tcp/TcpClient.java
@@ -3,9 +3,8 @@ package client.tcp;
 import java.io.*;
 import java.net.Socket;
 
-public class TcpClient implements Closeable{
-    private Socket socket;
-
+public class TcpClient {
+    private Socket socket = new Socket();
     private static final String DEFAULT_HOST = "0.0.0.0";
     private static final int DEFAULT_PORT = 4321;
 
@@ -13,17 +12,17 @@ public class TcpClient implements Closeable{
     private int port = DEFAULT_PORT;
 
     public TcpClient(String[] args) {
+
         if (args.length == 2) {
             host = args[0].trim();
             port = Integer.parseInt(args[1]);
         }
+
+        System.out.printf("Hello, what do you want to do?");
     }
 
     public void run() throws IOException {
         this.socket = new Socket(host, port);
-        CommandManager commandManager = new CommandManager();
-        ConnectionHandler connectionHandler = new ConnectionHandler(this.socket, commandManager);
-        connectionHandler.run();
     }
 
     public void close() {
