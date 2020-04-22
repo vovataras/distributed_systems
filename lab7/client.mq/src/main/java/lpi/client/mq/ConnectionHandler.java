f33546586217633aae5c0e4d3f738862e935c1c5
Volodya Taras <proger.sdk@gmail.com>
2020-04-22 18:20:16 +0300

    Lab_7: Add ping, echo

--- lab7/client.mq/src/main/java/lpi/client/mq/ConnectionHandler.java
+++ lab7/client.mq/src/main/java/lpi/client/mq/ConnectionHandler.java
@@ -4,7 +4,11 @@ import java.io.BufferedReader;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import javax.jms.JMSException;
 import javax.jms.Session;
+import javax.jms.Message;
+import javax.jms.TextMessage;
 
 
 public class ConnectionHandler implements Closeable {
@@ -70,12 +74,12 @@ public class ConnectionHandler implements Closeable {
     private void getResponse(String[] command) {
         try {
             switch ( command[0] ) {
-//                case "ping":
-//                    ping();
-//                    break;
-//                case "echo":
-//                    echo(command);
-//                    break;
+                case "ping":
+                    ping();
+                    break;
+                case "echo":
+                    echo(command);
+                    break;
 //                case "login":
 //                    login(command);
 //                    break;
@@ -118,6 +122,115 @@ public class ConnectionHandler implements Closeable {
             System.out.println("You need to login!\n");
             return false;
         }
+    }
+
+
+
+    private void ping() throws JMSException {
+        Message msg = session.createMessage(); // an empty message.
+
+        if (sendMessage(msg,"chat.diag.ping", "ping"))
+            System.out.println("Ping success!\n");
+//        else {
+//            // TODO:
+//            System.out.println("Ping error!\n");
+//        }
+    }
+
+
+
+    private void echo(String[] command) throws JMSException {
+        String[] echoMessage = Arrays.copyOfRange(command, 1, command.length);
+
+        TextMessage msg =
+                session.createTextMessage
+                        (String.join(" ", echoMessage)); // a message that contains a string.
+
+        if (!(sendMessage(msg, "chat.diag.echo", "echo")))
+            System.out.println("Unexpected error!\n");
+    }
+
+
+
+    private boolean sendMessage(Message msg, String queueName, String command) throws JMSException {
+        boolean isSuccess = false;
+
+        // create an object specifying the Destination to which the message will be sent:
+        javax.jms.Destination targetQueue = session.createQueue(queueName);
+
+        // create an object specifying the Destination where the response will be received:
+        javax.jms.Destination replyQueue = this.session.createTemporaryQueue();
+
+        // create a producer on targetQueue, which will allow to send the request message:
+        javax.jms.MessageProducer producer = session.createProducer(targetQueue);
+
+        // create a consumer on replyQueue, which will allow to receive the answer:
+        javax.jms.MessageConsumer consumer = session.createConsumer(replyQueue);
+
+        // specify JMSReplyTo attribute:
+        msg.setJMSReplyTo(replyQueue);
+
+        // send the message using producer:
+        producer.send(msg);
+
+        // await the reply using consumer:
+        Message replyMsg = consumer.receive(2000);
+
+
+        switch (command) {
+            case "ping":
+            case "exit":
+                if(!(replyMsg instanceof TextMessage)) {
+                    isSuccess = true;
+                }
+                break;
+            case "echo":
+                if(replyMsg instanceof TextMessage) {
+                    isSuccess = true;
+
+                    // expect the text message as the content
+                    String content = ((TextMessage) msg).getText(); // obtaining content.
+                    System.out.println(content + "\n");
+                }
+//                else { // oops, the message is not TextMessage.
+//                    throw new IOException("Unexpected message type: " + msg.getClass());
+//                }
+                break;
+
+
+//            case "login":
+//                if(!(msg instanceof TextMessage)) {
+//                    isSuccess = true;
+//
+////                    String content = null; // we expect the text message as the content.
+////                    if(msg instanceof TextMessage) { // therefore, we check for TextMessage.
+////                        content = ((TextMessage)msg).getText(); // obtaining content.
+////                    } else { // oops, the message is not TextMessage.
+////                        throw new IOException(“Unexpected message type: ” + msg.getClass());
+////                    }
+//                } else {
+//                    // we expect the text message as the content.
+//                    String content = ((TextMessage)msg).getText(); // obtaining content.
+//
+//                    System.out.println(content + "\n");
+//                }
+//                break;
+        }
+
+
+        // if unexpected input message
+        if (!isSuccess) {
+            // expect the text message as the error
+            String content = ((TextMessage)msg).getText(); // obtaining content
+            System.out.println(content + "\n");
+        }
+
+
+        // close producer and consumer:
+        consumer.close();
+        producer.close();
+
+        return isSuccess;
     }
 
 
